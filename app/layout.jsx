cd2f73f93f70ab11a485b35e661bafed64f3bba1
Protyay Roy <protyay@Protyays-Mac-mini.local>
2025-09-26 01:51:24 +0600

    Fix GTM tag firing and eliminate duplicate pixel events
    - Simplified pushToDataLayer() to single clean event push to GTM dataLayer
    - Removed direct Facebook and TikTok pixel calls from JavaScript code
    - Removed direct TikTok Pixel loading from layout.jsx
    - Eliminated alias events that were interfering with GTM triggers
    - GTM now manages all pixel events through triggers and tags
    - Resolves GTM tags not firing and duplicate Meta/TikTok pixel events
    - Improves performance by reducing JavaScript execution and network requests

--- app/layout.jsx
+++ app/layout.jsx
@@ -52,13 +52,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TW7XR5TX');` }} />
         {/* End Google Tag Manager */}
-        {/* Facebook Pixel is loaded via GTM to avoid duplicate activation */}
-        {/* TikTok Pixel */}
-        <script dangerouslySetInnerHTML={{ __html: `!function (w, d, t) { w.TiktokAnalyticsObject = t; var ttq = w[t] = w[t] || []; ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"], ttq.setAndDefer = function (t, e) { t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }; for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]); ttq.instance = function (t) { for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]); return e }, ttq.load = function (e, n) { var i = "https://analytics.tiktok.com/i18n/pixel/events.js"; ttq._i = ttq._i || {}; ttq._i[e] = []; ttq._i[e]._u = i; ttq._t = ttq._t || {}; ttq._t[e] = +new Date; ttq._o = ttq._o || {}; ttq._o[e] = n || {}; var o = document.createElement("script"); o.type = "text/javascript"; o.async = !0; o.src = i + "?sdkid=" + e + "&lib=" + t; var a = document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(o, a) };
-        }(window, document, 'ttq');
-        ttq.load('D37UPTBC77U411S6D6K0');
-        ttq.page();` }} />
-        {/* End TikTok Pixel */}
+        {/* Facebook and TikTok Pixels are loaded via GTM to avoid duplicate activation */}
         {/* Critical CSS inlined to prevent render blocking */}
         <style dangerouslySetInnerHTML={{
           __html: `
